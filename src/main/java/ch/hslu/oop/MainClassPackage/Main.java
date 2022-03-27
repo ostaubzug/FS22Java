852580515c74d5e1f1d52f717827ff8cdaccca2b
Oliver <ostaubzug@gmail.com>
2022-03-27 21:22:14 +0200

    Added Main

---
+++ src/main/java/ch/hslu/oop/MainClassPackage/Main.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ch.hslu.oop.MainClassPackage;
+
+/**
+ *
+ * @author ostau
+ */
+public class Main {
+    public static void main(String[] args){
+        
+        System.out.println("Hello World");
+    }
+    
+}
