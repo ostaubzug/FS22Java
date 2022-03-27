1b913f9e3e08217bbc4875e90bb642ba6ca997ba
ostaubzug <ostaubzug@gmail.com>
2022-03-27 20:53:24 +0200

    MainKlasse neu hinzugefügt

--- src/main/java/ch/hslu/oop/MainClassPackage/MainClass.java
+++ src/main/java/ch/hslu/oop/MainClassPackage/MainClass.java
@@ -2,21 +2,15 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package ch.hslu.oop.sw03;
+package ch.hslu.oop.MainClassPackage;
 
 /**
  *
  * @author ostau
  */
-public class Main {
+public class MainClass {
     
-    /**
-     *
-     * @param args
-     */
     public static void main(String[] args){
-        Point point = new Point(2,3);
-        System.out.println(point.getQuadrant());
         System.out.println("Hello World");
     }
     
