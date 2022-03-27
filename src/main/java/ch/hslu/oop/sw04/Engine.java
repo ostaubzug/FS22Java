70a372f8747b43e12bdfa8e3456f9b5a0e41f08e
Oliver <ostaubzug@gmail.com>
2022-03-27 21:20:48 +0200

    Added Engine

--- src/main/java/ch/hslu/oop/sw04/Engine.java
+++ src/main/java/ch/hslu/oop/sw04/Engine.java
@@ -8,6 +8,46 @@ package ch.hslu.oop.sw04;
  *
  * @author ostau
  */
-public class Engine {
+public class Engine implements ISwitchable{
+
+private int rpm = 0;
+    
+
+    @Override
+    public void switchOn()
+    {
+        rpm = 100;
+    }
+    
+
+    @Override
+    public void switchOff()
+    {
+        rpm = 0;
+    }
+    @Override
+    public boolean isSwitchedOn()
+    {
+        if(rpm >= 100)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    @Override
+    public boolean isSwitchedOff()
+    {
+        if(rpm <= 0)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
     
 }
