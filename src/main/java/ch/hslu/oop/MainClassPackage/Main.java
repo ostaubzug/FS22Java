ae14e74e8e81e529fc361a725e297c6872d06656
Oliver <ostaubzug@gmail.com>
2022-03-29 14:08:15 +0200

    SW06 - Point hinzugefügt

--- src/main/java/ch/hslu/oop/MainClassPackage/Main.java
+++ src/main/java/ch/hslu/oop/MainClassPackage/Main.java
@@ -1,12 +1,19 @@
 package ch.hslu.oop.MainClassPackage;
 
-import ch.hslu.oop.sw05.*;
+import ch.hslu.oop.sw06.*;
 
+/**
+ * 
+ */
 public class Main {
     public static void main(String[] args){
+        Point point = new Point(2,3);
+        Point point2 = new Point(point);
+        point2.moveRelative(point);
+        point.moveRelative(2,3);
+        System.out.println(point.toString());
+        System.out.println(point2.toString());
 
-        Nitrogen nitrogen = new Nitrogen();
-        System.out.println(nitrogen.getAggregateState(500));
+
     }
-    
 }
---
+++ src/main/java/ch/hslu/oop/sw06/Point.java
@@ -0,0 +1,84 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ch.hslu.oop.sw06;
+
+/**
+ * Beschreibt einen Punkt mit zwei Koordinaten. get Quadrant gibt den Quadranten
+ * an
+ * 
+ * @author Oliver Staub
+ */
+public class Point {
+    private int x;
+    private int y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public Point(Point point) {
+        this(point.getX(),point.getY());
+    }
+    
+    public void moveRelative(int x, int y) {
+        this.x += x;
+        this.y += y;
+    }
+
+    public void moveRelative(Point point) {
+        this.x += point.getX();
+        this.y += point.getY();
+    }
+
+    // schlechte Idee weil Signaturen sehr ähnlich sind
+    public void moveRelative(double angle, double amount) {
+        //TODO Stimmt noch nicht!!
+        //TODO Unit Tests schreiben
+        int xVector = (int) (angle * Math.cos(amount));
+        this.x += xVector;
+        int yVector = (int) (angle * Math.cos(amount));
+        this.y+= yVector;
+    }
+
+    public int getQuadrant() {
+        if (x > 0 && y > 0) {
+            return 1;
+        }
+
+        else if (x < 0 && y > 0) {
+            return 2;
+        }
+
+        else if (x < 0 && y < 0) {
+            return 3;
+        }
+
+        else if (x > 0 && y < 0) {
+            return 4;
+        }
+
+        else {
+            // Kann keinem Quadranten zugeordnet werden
+            return 0;
+        }
+    }
+
+    public int getX() {
+        int xCopy = x;
+        return xCopy;
+    }
+
+    public int getY() {
+        int yCopy = y;
+        return yCopy;
+    }
+
+    @Override
+    public String toString(){
+        return "Point ["+ x + " " + y + "]";
+    }
+
+}
